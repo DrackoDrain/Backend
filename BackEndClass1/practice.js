02bb52e5f5b80b68a5f9b10c49349bd0a8dfd1e3
Ayush <ayush0999aryan@gmail.com>
2024-01-28 00:48:23 +0530

    practice-sever

---
+++ BackEndClass1/practice.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+app.get('/', (req,res)=>{
+    res.send("HELLO this is ayush aryan ");
+})
+
+app.listen(4000, ()=>{
+    console.log(" 4000 port is running ");
+
+})
+
+app.post('/api/companies', (req, res) =>{
+    const {
+        name = "Gucci",
+        name1 = "Chanel"
+    } = req.body;
+    console.log(name)
+    console.log(name1)   
+
+    res.send("Companies get successfully");
+})
