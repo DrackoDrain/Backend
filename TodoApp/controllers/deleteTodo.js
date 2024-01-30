4d1b94b9a9ec3628ed59b26d484199b0a5619f2e
Ayush <ayush0999aryan@gmail.com>
2024-01-30 17:04:32 +0530

    deleteRequest

---
+++ TodoApp/controllers/deleteTodo.js
@@ -0,0 +1,25 @@
+const Todo = require("../models/Todo");
+
+
+exports.deleteTodo = async (req,res)=>{
+    try{
+        const {id} = req.params;
+
+        await Todo.findOneAndDelete(id);
+
+        res.json({
+            success:true,
+            message :"Todo Deleted",
+        })
+
+    }
+    catch(err){
+        console.error(err);
+        res.status(500)
+        .json({
+            success:false,
+            message:"Error in Deletion",
+        })
+
+    }
+}
--- TodoApp/routes/todo.js
+++ TodoApp/routes/todo.js
@@ -6,7 +6,8 @@ const router = express.Router();
 
 const {createTodo} = require("../controllers/createTodo");
 const {getTodo,getTodoById} = require("../controllers/getTodo");
-const {updateTodo} = require("../controllers/updateTodo")
+const {updateTodo} = require("../controllers/updateTodo");
+const {deleteTodo} = require("../controllers/deleteTodo");
 
 // define api routes 
 
@@ -14,6 +15,8 @@ router.post("/createTodo", createTodo);
 router.get("/getTodos", getTodo);
 router.get("/getTodos/:id",getTodoById);
 router.put("/updateTodo/:id",updateTodo);
+router.delete("/deleteTodo/:id",deleteTodo);
+
 
 module.exports = router;
 
