777b84c1093a3cf9ebf95bb78797c739bdff0b1d
Ayush <ayush0999aryan@gmail.com>
2024-01-30 13:32:47 +0530

    adeed getTodo in route

--- TodoApp/routes/todo.js
+++ TodoApp/routes/todo.js
@@ -5,6 +5,7 @@ const router = express.Router();
 // import controller
 
 const {createTodo} = require("../controllers/createTodo");
+const {getTodo} = require("../controllers/getTodo");
 
 // define api routes 
 
