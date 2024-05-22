0bcc3e64e7dd579d16e10d115083d11c32dbfd5a
Ayush <ayush0999aryan@gmail.com>
2024-05-22 11:35:46 +0530

    Socket-io implementation

---
+++ socketIO/index.js
@@ -0,0 +1,31 @@
+const http = require("http");
+
+const express = require("express")
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+
+// socket.io
+
+io.on('connection', (socket)=>{
+   socket.on("user-message", (message)=>{
+    io.emit("message", message);
+    
+   })
+});
+
+app.use(express.static(path.resolve("./public")));
+
+app.get('/', (req,res)=>{
+   return res.sendFile("/public/index.html");
+   
+})
+server.listen((9000), (req,res) =>{
+    console.log(`Server has started at 9000 port`);
+   
+});
+
